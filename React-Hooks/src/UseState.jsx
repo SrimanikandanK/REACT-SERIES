d7830b2f87544b4aa9116afa27229e4852df0ffc
oreoboy-max <srimani605@gmail.com>
2024-05-04 21:19:38 +0530

    usestate Hook

---
+++ React-Hooks/src/UseState.jsx
@@ -0,0 +1,19 @@
+import  { useState } from "react";
+import './App.css'
+
+const UseState=() => {
+  const [count, setCount] = useState(0)
+
+  function increaseCounter(){
+    setCount(count + 1);
+  }
+
+  return (
+    <>
+    <h1>Counter: {count}</h1>
+    <button onClick={increaseCounter}>Increase</button>
+    </>
+  )
+}
+
+export default UseState
--- React-Hooks/src/main.jsx
+++ React-Hooks/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import UseState from './UseState.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <UseState/> {/* Counter Program */}
+  </React.StrictMode>
 )
